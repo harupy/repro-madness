a73075c87b62b5baba0324607f3b11d6d02484b5
harupy <hkawamura0130@gmail.com>
2023-06-02 20:54:25 +0900

    a

--- example.py
+++ example.py
@@ -1,18 +1,20 @@
 from pyspark.sql import SparkSession
 import uuid
+import tempfile
 
-with (
-    SparkSession.builder.master("local[*]")
-    .config("spark.jars.packages", "io.delta:delta-core_2.12:2.0.2")
-    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
-    .config(
-        "spark.sql.catalog.spark_catalog",
-        "org.apache.spark.sql.delta.catalog.DeltaCatalog",
-    )
-    .getOrCreate()
-) as sess:
-    df = sess.createDataFrame(
-        [(i, uuid.uuid4().hex) for i in range(500)],
-        schema=["id", "value"],
-    )
-    df.coalesce(1).write.format("parquet").mode("append").save("data")
+with tempfile.TemporaryDirectory() as tempdir:
+    with (
+        SparkSession.builder.master("local[*]")
+        .config("spark.jars.packages", "io.delta:delta-core_2.12:2.0.2")
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+        .config(
+            "spark.sql.catalog.spark_catalog",
+            "org.apache.spark.sql.delta.catalog.DeltaCatalog",
+        )
+        .getOrCreate()
+    ) as sess:
+        df = sess.createDataFrame(
+            [(i, uuid.uuid4().hex) for i in range(500)],
+            schema=["id", "value"],
+        )
+        df.coalesce(1).write.format("parquet").mode("append").save(tempdir)
