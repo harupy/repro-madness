1d6278336444f5a176b5a92e5c9830afd7c89eff
harupy <hkawamura0130@gmail.com>
2023-06-02 12:12:01 +0900

    test
    Signed-off-by: harupy <hkawamura0130@gmail.com>

---
+++ example.py
@@ -0,0 +1,19 @@
+from pyspark.sql import SparkSession
+
+
+with (
+    SparkSession.builder.master("local[*]")
+    .config("spark.jars.packages", "io.delta:delta-core_2.12:2.4.0")
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+    .config(
+        "spark.sql.catalog.spark_catalog",
+        "org.apache.spark.sql.delta.catalog.DeltaCatalog",
+    )
+    .config("spark.sql.execution.arrow.pyspark.enabled", "true")
+    .getOrCreate()
+) as sess:
+    df = sess.createDataFrame(
+        [(1, "foo"), (2, "bar")],
+        schema=["id", "value"],
+    )
+    df.write.format("delta").mode("overwrite").saveAsTable("delta")
