0a97e1afe7d8c1194bad6febd36e86a4aec63dc7
harupy <hkawamura0130@gmail.com>
2023-06-04 13:54:57 +0900

    a
    Signed-off-by: harupy <hkawamura0130@gmail.com>

--- test_example.py
+++ test_example.py
@@ -10,41 +10,13 @@ from typing import Iterator, Tuple
 from pyspark.sql.functions import struct, col
 
 
-@pytest.fixture(scope="class", autouse=True)
+@pytest.fixture(scope="module")
 def spark():
-    with tempfile.TemporaryDirectory() as tmpdir:
-        with (
-            SparkSession.builder.master("local[*]")
-            .config("spark.jars.packages", "io.delta:delta-core_2.12:1.2.1")
-            .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
-            .config(
-                "spark.sql.catalog.spark_catalog",
-                "org.apache.spark.sql.delta.catalog.DeltaCatalog",
-            )
-            .config("spark.sql.warehouse.dir", str(tmpdir))
-            .getOrCreate()
-        ) as sess:
-            yield sess
+    with SparkSession.builder.master("local[*]").getOrCreate() as sess:
+        yield sess
 
 
-def test_save1(spark):
-    @pandas_udf("double")
-    def multiply(iterator: Iterator[pd.DataFrame]) -> Iterator[pd.Series]:
-        import datasets
-
-        for df in iterator:
-            yield df.sum(axis=1)
-
-    num_columns = 6
-    df = spark.createDataFrame(
-        [tuple(random.random() for _ in range(num_columns)) for i in range(500)],
-        schema=[str(i) for i in range(num_columns)],
-    )
-    df = df.withColumn("sum", multiply(struct([col(c) for c in df.columns])))
-    df.show()
-
-
-def test_save2(spark):
+def test_pandas_udf(spark):
     @pandas_udf("double")
     def multiply(iterator: Iterator[pd.DataFrame]) -> Iterator[pd.Series]:
         import datasets
