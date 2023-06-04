e8118a530c26e54a5ed96702ae974ad24423ffd5
harupy <hkawamura0130@gmail.com>
2023-06-04 13:58:10 +0900

    a
    Signed-off-by: harupy <hkawamura0130@gmail.com>

---
+++ example.py
@@ -0,0 +1,24 @@
+import random
+from typing import Iterator
+
+import pandas as pd
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import pandas_udf, struct, col
+
+
+with SparkSession.builder.master("local[*]").getOrCreate() as spark:
+
+    @pandas_udf("double")
+    def multiply(iterator: Iterator[pd.DataFrame]) -> Iterator[pd.Series]:
+        import datasets
+
+        for df in iterator:
+            yield df.sum(axis=1)
+
+    num_columns = 6
+    df = spark.createDataFrame(
+        [tuple(random.random() for _ in range(num_columns)) for i in range(500)],
+        schema=[str(i) for i in range(num_columns)],
+    )
+    df = df.withColumn("sum", multiply(struct([col(c) for c in df.columns])))
+    df.show()
